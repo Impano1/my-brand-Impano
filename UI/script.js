e88e0f0a3d863fe71cab570031c07f13d90fe956
Impano1 <f.impano@alustudent.com>
2024-02-18 17:13:22 +0200

    committing

--- UI/script.js
+++ UI/script.js
@@ -46,45 +46,97 @@ menuIcon.addEventListener('click', () => {
     navbar.classList.toggle('show');
 })
 
-const form = document.getElementById("contactform");
+
+const form = document.querySelector("form");
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+
+function sendEmail() {
+    const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${email.value}<br> Phone Number: ${phone.value}<br> Message: ${message.value}`;
+
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "lechretien200@gmail.com",
+        Password : "27AF7A3092BE6D4094A136E176A013941D1A",
+        To : 'lechretien200@gmail.com',
+        From : "lechretien200@gmail.com",
+        Subject : subject.value,
+        Body : bodyMessage
+    }).then(
+      message => {
+        if (message == "OK") {
+            Swal.fire({
+                title: "Thank you!",
+                text: "Your message has been sent!",
+                icon: "success"
+              });
+        }
+      }
+    );
+}
+
+function checkInputs() {
+    const items = document.querySelectorAll(".item");
+
+    for (const item of items) {
+        if (item.value == "") {
+            item.classList.add("error");
+            item.parentElement.classList.add("error");
+        }
+
+        if (items[1].value !== "") {
+            checkEmail();
+        }
+
+        items[1].addEventListener("keyup", () => {
+            checkEmail();
+        });
+
+        item.addEventListener("keyup", () => {
+            if(item.value != "") {
+                item.classList.remove("error");
+                item.parentElement.classList.remove("error");
+            }
+            else {
+                item.classList.add("error");
+                item.parentElement.classList.add("error");
+            }
+        })
+    }
+}
+
+function checkEmail() {
+    const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\. [a-z]{2,3})?$/;
+    const errorTxtEmail = document.querySelector(".error-txt.email");
+
+    if (!email.value.match(emailRegex)) {
+        email.classList.add("error");
+        email.parentElement.classList.add("error");
+
+        if (email.value != "") {
+            errorTxtEmail.innerText = "email address is not valid";
+        }
+        else {
+            errorTxtEmail.innerText = "email address is required";
+        }
+    }
+    else {
+        email.classList.remove("error");
+        email.parentElement.classList.remove("error");
+    }
+}
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-     validateForm()
+    checkInputs();
+
+    if (!email.classList.contains("error") && !fullName.classList.contains("error") && !phone.classList.contains("error") && !subject.classList.contains("error") && !message.classList.contains("error")) {
+        sendEmail();
+
+        form.reset();
+        return false;
+    }
 })
-
-function validateForm() {
-    console.log("working")
-    const fullName = document.getElementById("fullName").value.trim();
-    console.log(fullName)
-    const email = document.getElementById("email").value.trim();
-    const mobileNumber = document.getElementById("mobileNumber").value.trim();
-    const emailSubject = document.getElementById("emailSubject").value.trim();
-    const message = document.getElementById("message").value.trim();
-
-    if (fullName === "") {
-        alert("Please enter your full name");
-        return false;
-    }
-
-    if (email === "" || !validateEmail(email)) {
-        alert("Please enter a valid email address");
-        return false;
-    }
-
-    if (isNaN(mobileNumber) || mobileNumber.length !== 10) {
-        alert("Please enter a valid 10-digit mobile number");
-        return false;
-    }
-
-    if (emailSubject === "") {
-        alert("Please enter the email subject");
-        return false;
-    }
-
-    if (message === "") {
-        alert("Please enter your message");
-        return false;
-    }
-
-    return true;
-}
